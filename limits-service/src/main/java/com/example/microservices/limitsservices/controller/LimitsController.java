f0010408d6311ae429eac3692ec14ddf83abc507
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-02-14 16:24:04 +0300

    Add files via upload

---
+++ limits-service/src/main/java/com/example/microservices/limitsservices/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.example.microservices.limitsservices.controller;
+
+import com.example.microservices.limitsservices.configuration.Configuration;
+import com.example.microservices.limitsservices.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        //return new Limits(1, 1000);
+    }
+}
